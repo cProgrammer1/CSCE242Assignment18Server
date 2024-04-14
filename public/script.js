45dff3360828e7998c744e1df5303ad82b8749c2
cProgrammer1 <chrissy.dobrowolski@gmail.com>
2024-04-14 18:56:50 -0400

    Assignment 18; 4th commit

--- public/script.js
+++ public/script.js
@@ -26,8 +26,8 @@ const getCrafts = async() =>
 {
     try
     {
-        // let response = await fetch("http://localhost:3000/api/crafts");
-        let response = await fetch("https://csce242assignment18server.onrender.com/api/crafts");
+        let response = await fetch("http://localhost:3000/api/crafts");
+        // let response = await fetch("https://csce242assignment18server.onrender.com/api/crafts");
         return (await fetch("api/crafts/")).json();
 
     }
